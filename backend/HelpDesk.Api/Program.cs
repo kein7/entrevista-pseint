e6c19bec7ab0b5d10f8fe78c36c69fa9530ad7af
kevin <kevin.o.nicolas.m@gmail.com>
2025-12-26 16:25:53 -0300

    feat: inicializacion del jwt en el program.cs

--- backend/HelpDesk.Api/Program.cs
+++ backend/HelpDesk.Api/Program.cs
@@ -1,7 +1,20 @@
 using HelpDesk.Infrastructure; // Importante para ver el método de extensión
 using HelpDesk.Infrastructure.Persistence;
+using Serilog;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/helpdesk-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+// Decirle a .NET que use Serilog en lugar del logger por defecto
+builder.Host.UseSerilog();
 
 // 1. Agregar servicios de las capas inferiores
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -21,7 +34,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); // Requisito 
 
+// Auth JWT config
+DotNetEnv.Env.Load();
+if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JWT_KEY")))
+{
+    // Esto te ayudará a debuguear si el archivo no se encuentra
+    throw new Exception("ERROR: La variable JWT_KEY no se encontró. Verifica el archivo .env");
+}
+var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_KEY") ?? "");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+        ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+
+// app build
 var app = builder.Build();
+Log.Information("La API de HelpDesk ha iniciado correctamente."); 
 
 app.UseMiddleware<HelpDesk.Api.Middleware.ExceptionMiddleware>();
 
@@ -35,6 +79,8 @@ if (app.Environment.IsDevelopment())
 app.UseCors("AngularPolicy"); // Aplicar CORS 
 app.UseAuthentication(); // Para el requisito de login 
 app.UseAuthorization();
+
+
 
 // Aplicar migraciones automáticamente al iniciar
 using (var scope = app.Services.CreateScope())
