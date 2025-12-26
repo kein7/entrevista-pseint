13063f6c27fbe950e82898b218ca4dbf477fe269
kevin <kevin.o.nicolas.m@gmail.com>
2025-12-26 15:37:49 -0300

    feat: add db context

---
+++ backend/HelpDesk.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpDesk.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelpDesk.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Configuración de SQLite 
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
