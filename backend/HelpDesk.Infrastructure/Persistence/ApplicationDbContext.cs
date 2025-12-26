48cec8041f62f5bcf811ff048d1cfe808ff94810
kevin <kevin.o.nicolas.m@gmail.com>
2025-12-26 13:04:28 -0300

    feat: configuracion de la base de datos

---
+++ backend/HelpDesk.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using HelpDesk.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Ticket> Tickets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuración adicional si fuera necesaria
+        modelBuilder.Entity<Ticket>(entity => {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Title).IsRequired().HasMaxLength(100);
+        });
+    }
+}
