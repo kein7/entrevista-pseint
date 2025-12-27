f8fdf769b3690e8b52ab628459dad99b91931c6e
kevin <kevin.o.nicolas.m@gmail.com>
2025-12-27 01:26:45 -0300

    feat: mostrar error de formulario

--- helpdesk-frontend/src/app/features/tickets/ticket-form/ticket-form.ts
+++ helpdesk-frontend/src/app/features/tickets/ticket-form/ticket-form.ts
@@ -9,37 +9,50 @@ import { TicketService, Ticket } from '../../../core/services/ticket';
   templateUrl: './ticket-form.html',
   styleUrl: './ticket-form.scss',
 })
-export class TicketFormComponent {
+export class TicketForm {
   private fb = inject(FormBuilder).nonNullable;
   private ticketService = inject(TicketService);
 
-  // Nuevo en Angular 17.3+: Output con función output()
   ticketCreated = output<Ticket>();
   isLoading = signal(false);
+  errorMessage = signal<string | null>(null);
 
   ticketForm = this.fb.group({
     title: ['', [Validators.required, Validators.minLength(5)]],
     description: ['', [Validators.required]],
     priority: [1, [Validators.required]],
+    assignedUser: ['admin', [Validators.required]],
   });
 
   onSubmit() {
     if (this.ticketForm.invalid) return;
 
     this.isLoading.set(true);
+    this.errorMessage.set(null);
+
     const formValue = this.ticketForm.getRawValue();
-    const ticketToCreate: Ticket = {
-      title: formValue.title ?? '',
-      description: formValue.description ?? '',
-      priority: Number(formValue.priority) ?? 1,
+
+    const ticketToCreate = {
+      title: formValue.title,
+      description: formValue.description,
+      priority: Number(formValue.priority), // <--- CRÍTICO: Convertir string a number
+      assignedUser: formValue.assignedUser,
+      status: 0, // Asumimos 0 como 'Abierto' por defecto
     };
-    this.ticketService.createTicket(ticketToCreate).subscribe({
+    this.ticketService.createTicket(ticketToCreate as Ticket).subscribe({
       next: (newTicket) => {
-        this.ticketCreated.emit(newTicket); // Notificamos al padre
-        this.ticketForm.reset({ priority: 1 });
+        this.ticketCreated.emit(newTicket);
+        this.ticketForm.reset({ priority: 1, assignedUser: 'admin' });
         this.isLoading.set(false);
       },
-      error: () => this.isLoading.set(false),
+      error: (err) => {
+        this.isLoading.set(false);
+        if (err.status === 400) {
+          this.errorMessage.set('Revisa los campos obligatorios.');
+        } else {
+          this.errorMessage.set('Ocurrió un error inesperado al conectar con el servidor.');
+        }
+      },
     });
   }
 }
