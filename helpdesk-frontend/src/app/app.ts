7f240819683ea6c1da4e9bbd08e38d0bfe5edfcc
kevin <kevin.o.nicolas.m@gmail.com>
2025-12-26 22:08:07 -0300

    feat: se añadio el header a las rutas protegidas

--- helpdesk-frontend/src/app/app.ts
+++ helpdesk-frontend/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header';
+import { AuthService } from './core/services/auth';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('helpdesk-frontend');
+  authService = inject(AuthService);
 }
